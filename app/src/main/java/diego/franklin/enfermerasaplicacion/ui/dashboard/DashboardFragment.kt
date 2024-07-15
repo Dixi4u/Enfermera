79b4243ee08fc604f5e6c71a2189a4498744cbed
Dixi <20200201@ricaldone.edu.sv>
2024-07-14 20:49:58 -0600

    Profe Bryan mi pastor

--- app/src/main/java/diego/franklin/enfermerasaplicacion/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/diego/franklin/enfermerasaplicacion/ui/dashboard/DashboardFragment.kt
@@ -28,7 +28,7 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textView2
+        val textView: TextView = binding.tvMensaje
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
